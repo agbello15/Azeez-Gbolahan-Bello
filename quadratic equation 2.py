9b006701f3c2bd4989819d9c017649c38e3657c2
agbello15 <45173603+agbello15@users.noreply.github.com>
2018-11-26 03:43:20 -0800

    Add files via upload

---
+++ quadratic equation 2.py
@@ -0,0 +1,13 @@
+import math
+
+x=int(input("give me a number"))
+y= int(input("give me a number"))
+print(x+y)
+a=float(input("give me the value of a"))
+b=float(input("give me the value of b"))
+c=float(input("give me the value of c"))
+d=math.sqrt(b**2)-(4*a*c)
+print(d)
+x=(-b+d)/(2*a)
+print(x)
+
